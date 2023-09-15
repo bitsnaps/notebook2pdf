471a24f43bc6092eace0fa15379e4e6995c3cc14
Ibrahim.H <bitsnaps@yahoo.fr>
2023-09-15 20:40:15 +0000

    reduce nbr of depdencies

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ app = FastAPI()
 def read_root():
     return FileResponse("index.html")
 
-
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
-    with open(file.filename, "wb") as f:        
+    file_path = os.path.join('/tmp', file.filename)
+    file_name = file.filename.lower().replace('ipynb', 'pdf')    
+    with open(file_path, "wb") as f:    
         try:
             content = await file.read()
             if not file.filename.endswith('.ipynb'):
@@ -23,17 +24,18 @@ async def upload_file(file: UploadFile = File(...)):
                 return {"error": "Invalid Jupyter Notebook file. Please upload a valid .ipynb file."}
             
             f.write(content)
-            subprocess.run(["jupyter", "nbconvert", "--to", "pdf", file.filename])
+            print(f"file_name: {file_name}, file_path: {file_path}")
+            subprocess.run(["jupyter", "nbconvert", "--to", "pdf", file_path])
         except json.JSONDecodeError as e:
             return {"error": f"Invalid JSON format. Please upload a valid Jupyter Notebook file (.ipynb): {str(e)}"}
         except Exception as e:
             return {"error": f"Invalid file: {str(e)}"}
     
-    file_name = file.filename.lower().replace('ipynb', 'pdf')
-    file_location = os.path.basename(file_name)
+    file_location = os.path.join('/tmp', os.path.basename(file_name))
+    print(f"file_name: {file_name}, file_location: {file_location}")
 
     # Asynchronously delete the file after 5 minutes
-    asyncio.create_task(delete_file_after_delay([file.filename, file_name], 5*60))
+    asyncio.create_task(delete_file_after_delay([file.filename, file_location], 5*60))
 
     if os.path.isfile(file_location):
         return FileResponse(file_location, media_type='application/pdf',filename=file_name)
